2b3f042bcf81b060477df82410d3f9c883ae17cf
joaopedrocastilhocardoso <95507750+joaopedrocastilhocardoso@users.noreply.github.com>
2025-09-18 20:56:52 -0300

    Update CronometerController.vhd

--- CronometerController/CronometerController.vhd
+++ CronometerController/CronometerController.vhd
@@ -4,35 +4,35 @@ use IEEE.numeric_std.all;
 
 entity CronometerController is
     port (
-        btn_clear   : in  std_logic;
-        btn_pause   : in  std_logic;
-        btn_reset   : in  std_logic;
-        current_state : out std_logic_vector(1 downto 0)
+        btn_pause        : in  std_logic;
+        btn_reset        : in  std_logic;
+        current_state    : out std_logic;
+		  cronometer_reset : out std_logic
     );
 end entity CronometerController;
 
 architecture behavioral of CronometerController is
 
-    signal internal_state : std_logic_vector(1 downto 0) := "00";
-    -- 00 - Reset
-    -- 01 - Active
-    -- 10 - Paused
+    signal internal_state : std_logic := '0';
+	 signal internal_reset : std_logic := '0';
+    -- 0 - Paused
+    -- 1 - Active
 
 begin
 
-    process(btn_reset, btn_clear, btn_pause)
+    process(btn_reset, btn_pause)
     begin
-        if btn_reset = '1' then
-            internal_state <= "00";
-        elsif btn_clear = '0' and internal_state = "10" then
-            internal_state <= "00";
-        elsif btn_pause = '0' and internal_state = "01" then
-            internal_state <= "10";
-        elsif btn_pause = '0' and (internal_state = "10" or internal_state = "00") then
-            internal_state <= "01";
+        if (btn_reset = '0' and internal_state = '0') then
+				internal_reset <= '1';
+        elsif btn_pause = '0' and internal_state = '0' then -- Se o cronômetro está pausado
+				internal_reset <= '0';
+            internal_state <= '1';
+        elsif btn_pause = '0' and (internal_state = '1') then -- Se o cronômetro está ativo
+            internal_state <= '0';
         end if;
     end process;
 
     current_state <= internal_state;
+	 cronometer_reset  <= internal_reset;
 
 end behavioral;
