807fb8b0a58414ff7fb96069e4ff2397d609e795
Matheus Passos <vpsmatheus@hotmail.com>
2025-09-24 09:04:39 -0300

    feat: implements letter b, 1, 2,3,4 and 5 numbers

--- Totalizadores/Totalizadores.vhd
+++ Totalizadores/Totalizadores.vhd
@@ -19,8 +19,81 @@ end entity;
 
 
 Architecture A_Totalizadores of Totalizadores is
-	
+
+	signal b0, b1, b2, b3 : unsigned(2 downto 0);
+
+begin
+
+	-- sinais auxiliares (CASE/WHEN) para cada bit
+	with inputWord(0) select b0 <=
+		"001" when '1',
+		"000" when others;
+
+	with inputWord(1) select b1 <=
+		"001" when '1',
+		"000" when others;
+
+	with inputWord(2) select b2 <=
+		"001" when '1',
+		"000" when others;
+
+	with inputWord(3) select b3 <=
+		"001" when '1',
+		"000" when others;
+
+	process(clk)
+		variable cnt_for   : unsigned(2 downto 0);
+		variable cnt_while : unsigned(2 downto 0);
+		variable cnt_if    : unsigned(2 downto 0);
+		variable i         : integer range 0 to 4;
+		variable w         : integer range 0 to 4;
 	begin
-	
-	
+		if rising_edge(clk) then
+			-- b.1) VARIABLES FOR
+			cnt_for := (others => '0');
+			for i in 0 to 3 loop
+				if inputWord(i) = '1' then
+					cnt_for := cnt_for + 1;
+				end if;
+			end loop;
+			variablesForOut <= cnt_for;
+
+			-- b.2) VARIABLES WHILE
+			cnt_while := (others => '0');
+			w := 0;
+			while w <= 3 loop
+				if inputWord(w) = '1' then
+					cnt_while := cnt_while + 1;
+				end if;
+				w := w + 1;
+			end loop;
+			variablesWhileOut <= cnt_while;
+
+			-- b.3) VARIABLES (IF then)
+			cnt_if := (others => '0');
+			if inputWord(0) = '1' then
+				cnt_if := cnt_if + 1;
+			end if;
+			if inputWord(1) = '1' then
+				cnt_if := cnt_if + 1;
+			end if;
+			if inputWord(2) = '1' then
+				cnt_if := cnt_if + 1;
+			end if;
+			if inputWord(3) = '1' then
+				cnt_if := cnt_if + 1;
+			end if;
+			variablesIfThen <= cnt_if;
+
+			-- b.4) SIGNALS (case/when): somatório dos 4 sinais auxiliares
+			signalsCaseWhenOut <= b0 + b1 + b2 + b3;
+
+			-- b.5) SIGNALS (soma direta com conversão direta dos bits de entrada)
+			signalsDirectSumOut <= unsigned("00" & inputWord(0)) +
+							     unsigned("00" & inputWord(1)) +
+							     unsigned("00" & inputWord(2)) +
+							     unsigned("00" & inputWord(3));
+		end if;
+	end process;
+
 End Architecture;
